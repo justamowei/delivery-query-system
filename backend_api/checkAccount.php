1ed8804cbc498b156f51035fd7012e202a83e164
JustinSsu <ssu.pei.je@gmail.com>
2024-12-14 14:16:59 +0800

    update CORS problem

--- backend_api/checkAccount.php
+++ backend_api/checkAccount.php
@@ -1,4 +1,9 @@
 <?php
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
 $conn = require_once 'db_connection.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
